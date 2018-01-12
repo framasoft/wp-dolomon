8ebaaeee124f0deadae7a23b167ec95f86c69366
Luc Didry <luc@didry.org>
2018-01-12 08:01:45 +0100

    Headers update

--- dolomon.php
+++ dolomon.php
@@ -11,12 +11,12 @@
     * License URI: http://www.gnu.org/licenses/gpl.txt
     * Domain Path: /languages
     *
-    * {Plugin Name} is free software: you can redistribute it and/or modify
+    * Dolomon is free software: you can redistribute it and/or modify
     * it under the terms of the GNU General Public License as published by
     * the Free Software Foundation, either version 3 of the License, or
     * any later version.
     *
-    * {Plugin Name} is distributed in the hope that it will be useful,
+    * Dolomon is distributed in the hope that it will be useful,
     * but WITHOUT ANY WARRANTY; without even the implied warranty of
     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     * GNU General Public License for more details.
