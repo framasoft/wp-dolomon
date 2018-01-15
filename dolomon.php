5d4aac30672c325f63104cdc44552c4b064462d3
Luc Didry <luc@didry.org>
2018-01-15 14:58:28 +0100

    Bump version

--- dolomon.php
+++ dolomon.php
@@ -4,7 +4,7 @@
     * Plugin Name: Dolomon
     * Plugin URI: https://framagit.org/framasoft/wp-dolomon
     * Description: Transforms an URL into an URL provided by a Dolomon server, which creates visit statistics
-    * Version: 1.0
+    * Version: 1.0.1
     * Author: Framasoft
     * Author URI: https://framasoft.org
     * License: GPLv3
